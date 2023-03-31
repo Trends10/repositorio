cd26f9695dc3c96ebedcdb8b8a063865caa65f55
flavio silva <flavyossilva06@gmail.com>
2023-03-31 17:53:13 -0300

    exercicio 3 java

---
+++ trybe-exercicios/Fundamentos/secao-03-introdução-a-javaScript/dia-01-JavaScript-Primeiros-passos/exercicio3.js
@@ -0,0 +1,11 @@
+let a = 10;
+let b = 8;
+let c = 4;
+
+if (a > b && a > c) {
+  console.log('O maior número é: ' + a + ' (a)');
+} else if (b > a && b > c) {
+  console.log('O maior número é: ' + b + ' (b)');
+} else {
+  console.log('O maior número é: ' + c + ' (c)');
+};
